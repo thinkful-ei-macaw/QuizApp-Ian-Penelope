b9e26b51ef073d9878919005806627551aa55961
MediocreIan <idrews094@gmail.com>
2020-02-14 13:10:54 -0600

    added functonality for submiting questions and restructured DATA object

--- script.js
+++ script.js
@@ -11,161 +11,188 @@
 //Users should be shown their overall score at the end of the quiz. In other words, how many questions they got right out of the total questions asked.
 //users should be able to start a new quiz.
 
-const DATA = [
+const DATA = {
+  questions : [
   {
-    question: 'What\'s Carl Grimes Mother\'s name?',
-    a : 'Lori',
-    b : 'Carol',
-    c : 'Michonne',
-    d : 'Rosita',
-    questionNum : 1,
+    question: `What's Carl Grimes Mother's name?`,
+    answers: [
+      'Lori',
+      'Carol',
+      'Michonne',
+      'Rosita'],
     id : cuid(),
     correctAnswer : 'Lori'
   },
   {
     question : 'How did the Merle escape the roof top?',
-    a : 'jump off the roof',
-    b : 'screams for help',
-    c :  'He does\'t die',
-    d  : 'Cuts off his hand',
-    questionNum : 2,
+    answers: [
+      'jump off the roof',
+      'screams for help',
+      'He does\'t die',
+      'Cuts off his hand'],
     id : cuid(),
     correctAnswer : 'Cuts off his hand'
   },
   { question: 'In what season, did Morgan notice Rick Grimes was still Alive?',
-    a: 'Season 2',
-    b: 'Season 3',
-    c: 'Season 4',
-    d: 'Season 5',
-    questionNum : 3,
+    answers : [
+    'Season 2',
+    'Season 3',
+    'Season 4',
+    'Season 5'],
     id : cuid(),
     correctAnswer : 'Season 5'
   },
   { question:'Who did Lori had an affair with?',
-    a: 'Daryl',
-    b: 'Hershel',
-    c: 'Shane',
-    d: 'Merle',
-    questionNum : 4,
+    answers : [
+      'Daryl',
+      'Hershel',
+      'Shane',
+      'Merle'],
     id : cuid(),
     correctAnswer : 'Shane'
   },
   { question: 'When Carl got shot, who injured him?',
-    a:'Otis',
-    b:'Maggi' ,
-    c: 'Glenn',
-    d:'T-dog',
-    questionNum : 5,
+    answers : [
+      'Otis',
+      'Maggi' ,
+      'Glenn',
+      'T-dog'],
     id : cuid(),
     correctAnswer : 'Otis'
   },
   { question: 'How does Lori die?',
-    a: 'Drowning',
-    b: 'child-birth',
-    c: 'gun shot',
-    d: 'walkers attack.', 
-    questionNum : 6,
+    answers : [
+    'Drowning',
+    'child-birth',
+    'gun shot',
+    'walkers attack.'],
     id : cuid(),
     correctAnswer : 'child-birth' 
   },
   {question: 'Who get left behind at the farm?',
-    a:'Glenn',
-    b: 'Maggie',
-    c: 'Beth',
-    d: 'Andrea',
-    questionNum : 7,
+    answers: [
+    'Glenn',
+    'Maggie',
+    'Beth',
+    'Andrea'
+    ],
     id : cuid(),
     correctAnswer : 'Andrea'
   },
   { question: 'In what season did the group run into Terminus?',
-    a: 'Season 1',
-    b:  'Season 3',
-    c:  'Season 5',
-    d:  'Season 4',
-    questionNum : 8,
+  answers : [
+    'Season 1',
+    'Season 3',
+    'Season 5',
+    'Season 4'
+  ],
     id : cuid(),
     correctAnswer : 'Season 5'
   },
   {question: 'How did the Governor kill Hershal?',
-    a: 'gun shot to the head',
-    b: 'Governor fed him to the Walkers',
-    c: 'Governor cuts his head off',
-    d:  'Governor threw him the pit',
-    questionNum : 9,
+    answers: [
+    'gun shot to the head',
+    'Governor fed him to the Walkers',
+    'Governor cuts his head off',
+    'Governor threw him the pit'
+  ],
     id : cuid(),
     correctAnswer : 'Governor cuts his head off'
   },
   {question: 'In what season did Rick renounce Leadership of the Group?',
-    a: 'Season 1',
-    b: 'Season 2',
-    c: 'Season 4',
-    d: 'Season 3',
-    questionNum : 10,
+    answers: [
+    'Season 1',
+    'Season 2',
+    'Season 4',
+    'Season 3'
+    ],
     id : cuid(),
     correctAnswer : 'Season 4'
   },
-  {questionNumber: 8},
-  {score: 0}
+],
+  questionNumber: 5,
+  score: 0
 
-];
+};
 
 
 
 function renderPage() {
   //this function will render the page based on the data in DATA.
+  if (DATA.questionNumber === 0) {
+    let page = startScreen()
+    $('body').html(page)
+  }
   console.log('renderPage working');
 }
 
 function startScreen() {
   //this will generate the start screen HTML
-  $('body').html(`<header>
+  return `<header>
   <h1>Welcome to the Walking Dead</h1>
 </header>
 <p class="instructions">
   Click Begin Quiz To See How Much You Know About Characters From The Walking Dead
 </p>
-<input type="button" class="start-quiz js-start-button" value="begin">`);
+<input type="button" class="start-quiz js-start-button" value="begin">`;
 }
 
 function generateQuestion() {
   //call on the data for a question in the DATA array and use it to generate HTML of each question including the question they are on out of the total
-  let i = DATA[10].questionNumber;
+  let i = DATA.questionNumber;
   $('body').html(`<p class = "question-box">Question 1</p>
-  <div class = "question-box">
-    <p>${DATA[i].question}</p>
-      <input type="radio" class="option">
-        <label for="answerA">${DATA[i].a} </label><br>
-      <input type="radio" class="option">
-        <label for="answerB">${DATA[i].b}</label><br>
-      <input type="radio" class="option">
-        <label for="answerC">${DATA[i].c}</form><label>
-      <input type="radio" class="option">
-        <label for="answerD">${DATA[i].d}</form><label>
-  </div>`);
+  <form class = "question-box">
+    <p>${DATA.questions[i].question}</p>
+        <label for="answerA">${DATA.questions[i].answers[0]}
+          <input type="radio" class="option" id = "answerA" name= "answers">
+        </label>
+      
+        <label for="answerB">${DATA.questions[i].answers[1]}
+          <input type="radio" class="option" id = "answerB" name = "answers">
+        </label>
+      
+        <label for="answerC">${DATA.questions[i].answers[2]}
+          <input type="radio" class="option" id = "AnswerC" name= "answers">
+        <label>
+      
+        <label for="answerD">${DATA.questions[i].answers[3]}
+          <input type="radio" class="option" id = "answerD" name = "answers">
+        <label>
+  </form>`);
   console.log('generateQuestion working');
 }
 
-function answerQuestions() {
-  // allow the user to answer a question with radio buttons and send the value selsected to the checkAnswer function
-  console.log('answerQuestions working'); 
-  $(document).on('click', '.option', function(event) {
-    console.log('WORKING');
-    let result = checkAnswer(event);
-    if (result === true) {
-      displayCorrect()
-    } else {
-      displayWrong()
+function handleRadioClicks() {
+  $(document).on('click','.option' ,function(e) {
+    console.log('handleRadioClicks WORKING');
+    let id = $(e.target).attr('id');
+    let selectedAnswer = $(`label[for='${id}']`).text();
+    answerQuestions(selectedAnswer)
     }
-  });
+  );
 }
 
-function checkAnswer(event) {
-  //take the data from answer question and compare it with the correct answer corresponding in DATA and trigger either displayCorrect or displayWrong depending
-  let i = DATA[10].questionNumber;
-  let ans = this.closest('label');
-  console.log(ans);
-  console.log('checkAnswer working');
+function answerQuestions(answer) {
+
+  let result = checkAnswer(answer);
+  console.log(answer)
+  if (result === true) {
+    displayCorrect();
+  } else {
+    displayWrong()
+  }
+  console.log('answerQuestions working'); 
+  
 }
+
+function checkAnswer(answer) {
+  let i = DATA.questionNumber;
+  let correct = DATA.questions[i].correctAnswer
+  if (answer === correct) {
+    return true
+  } else {return false}
+  console.log('checkAnswer working');
+}  // event selector not working
 
 function displayCorrect() {
   // display HTML for correct answer including a button to go to the next answer
@@ -194,5 +221,7 @@ function main() {
   nextQuestion();
   checkAnswer();
   answerQuestions();
+  handleRadioClicks();
+  renderPage();
 }
 $(main);
